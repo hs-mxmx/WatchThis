6121695ad5e3bfe1f9896877b696de29ce8e0f40
hs-mxmx <d.perezmoreno@outlook.es>
2021-01-26 17:42:38 +0100

    Backend && db configuration Factory/Decorator patterns

--- controller/database/operations/seasons_operations.py
+++ controller/database/operations/seasons_operations.py
@@ -1,31 +1,59 @@
 from flask import request, Response
 from database.models.seasons_models import Seasons
+import json
 
 
-def get_seasons():
-    seasons = Seasons.objects().to_json()
-    return seasons
+def init_seasons(app):
 
+    @app.route("/seasons")
+    def get_seasons():
+        try:
+            seasons = Seasons.objects().to_json()
+        except(Exception):
+            message = {"Error": "Invalid get action for seasons, please check URL and METHOD for request."}
+            print(message)
+            return message
+        return Response(seasons, mimetype="application/json", status=200)
 
-def add_seasons_body():
-    body = request.get_json()
-    print(body)
-    seasons = Seasons(**body).save()
-    id = seasons.id
-    return {'id': str(id)}, 200
+    @app.route('/seasons', methods=['POST'])
+    def add_seasons_body():
+        try:
+            body = request.get_json()
+            season = Seasons(**body).save().to_json()
+            season = json.loads(season)
+        except(Exception):
+            message = {"Error": "Invalid add action request for seasons, please check URL, METHOD, or data TYPE for request."}
+            print(message)
+            return message
+        return {"Add action": {"id": season["_id"]["$oid"], "season": season["name"]}}
 
+    @app.route('/seasons/<id>', methods=['PUT'])
+    def update_seasons(id):
+        try:
+            body = request.get_json()
+            Seasons.objects.get(id=id).update(**body)
+        except(Exception):
+            message = {"Error": "Invalid update action for seasons, please check URL and METHOD for request."}
+            print(message)
+            return message
+        return {"Update action": {"season": body["name"]}}
 
-def update_seasons(id):
-    body = request.get_json()
-    Seasons.objects.get(id=id).update(**body)
-    return body['name']
+    @app.route('/seasons/<id>', methods=['DELETE'])
+    def delete_season(id):
+        try:
+            Seasons.objects.get(id=id).delete()
+        except(Exception):
+            message = {"Error": "Invalid delete action for seasons, please check URL and METHOD for request."}
+            print(message)
+            return message
+        return {"Delete action": {"id": id}}
 
-
-def delete_seasons(id):
-    Seasons.objects.get(id=id).delete()
-    return id
-
-
-def get_season(id):
-    seasons = Seasons.objects.get(id=id).to_json()
-    return Response(seasons, mimetype="application/json", status=200)
+    @app.route('/seasons/<id>', methods=['GET'])
+    def get_season(id):
+        try:
+            seasons = Seasons.objects.get(id=id).to_json()
+        except(Exception):
+            message = {"Error": "Invalid get action for selected season, please check URL and METHOD for request."}
+            print(message)
+            return message
+        return Response(seasons, mimetype="application/json", status=200)
