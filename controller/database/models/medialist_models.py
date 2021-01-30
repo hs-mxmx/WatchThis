6053355af9e126d3beefeffbe0fcaf2577b03fd3
hs-mxmx <d.perezmoreno@outlook.es>
2021-01-30 18:56:00 +0100

    Search bar functionality && anime models

--- controller/database/models/medialist_models.py
+++ controller/database/models/medialist_models.py
@@ -2,5 +2,6 @@ from database.db import db
 
 
 class MediaList(db.Document):
+    type = db.StringField(required=True)
     name = db.StringField(required=True)
     medialist = db.ListField(db.StringField(), required=True)
