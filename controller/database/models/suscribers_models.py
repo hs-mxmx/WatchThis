107515d718a2d11c3ee187fee32e63f0dde1a91d
hs-mxmx <d.perezmoreno@outlook.es>
2021-02-04 12:29:28 +0100

    Suscriber unique email false

--- controller/database/models/suscribers_models.py
+++ controller/database/models/suscribers_models.py
@@ -2,5 +2,5 @@ from database.db import db
 
 
 class Suscribers(db.Document):
-    email = db.StringField(required=True, unique=True)
+    email = db.StringField(required=True, unique=False)
     genre = db.StringField(required=True)
